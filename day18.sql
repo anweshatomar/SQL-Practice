c18c181f73ed71de25062e7a51a3c75a59ab3987
Anwesha Tomar <atomar19@gwu.edu>
2021-05-13 15:43:27 -0400

    Create day18.sql

---
+++ day18.sql
@@ -0,0 +1,33 @@
+Link:https://leetcode.com/problems/delete-duplicate-emails/
+
+/**
+
+Day 18-
+
+Write a SQL query to delete all duplicate email entries in a table named Person, keeping only unique emails based on its smallest Id.
+
++----+------------------+
+| Id | Email            |
++----+------------------+
+| 1  | john@example.com |
+| 2  | bob@example.com  |
+| 3  | john@example.com |
++----+------------------+
+Id is the primary key column for this table.
+For example, after running your query, the above Person table should have the following rows:
+
++----+------------------+
+| Id | Email            |
++----+------------------+
+| 1  | john@example.com |
+| 2  | bob@example.com  |
++----+------------------+
+Note:
+
+Your output is the whole Person table after executing your sql. Use delete statement.
+
+**/
+
+Solution:
+
+delete l1 from person l1 inner join person l2 on l1.id>l2.id and l1.email=l2.email; 
